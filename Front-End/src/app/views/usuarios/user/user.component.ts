bccf3d2d4b827a9debd0cd3647dc92f53ec90912
Axel <axelguty@hotmail.com>
2024-05-13 17:08:52 -0500

    Interfaz de usuario

--- Front-End/src/app/views/usuarios/user/user.component.ts
+++ Front-End/src/app/views/usuarios/user/user.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,70 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  userArray: any[] = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.loadAllUser();
+  }
 
+  loadAllUser() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((res: any) => {
+        this.userArray = res;
+      });
+  }
+  originalValue: any;
+  onEdit(item: any) {
+    this.originalValue={...item};
+    item.isEdit = true;
+  }
+  onUpdate(data: any): void {
+      console.log(data);
+  }
+
+  onRowUpdate(user:any):void{
+    
+    if (this.originalValue && JSON.stringify(user) !== JSON.stringify(this.originalValue)) {
+      // El valor ha cambiado    
+      this.onUpdate(user);
+    }
+    user.isEdit = !user.isEdit;
+  }
+  validateField(item: any) {
+    if (item !== '') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  validateUserName(userName: string) {
+    if (userName === '') {
+      return 'Required';
+    } else {
+      if (userName.length >= 3) {
+        return '';
+      } else {
+        return 'min 3 char';
+      }
+    }
+  }
+  validateForm(obj: any) {
+    if (obj.name != '' && obj.username != '' && obj.phone != '') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  onCancel(item: any) {
+    item.isEdit = false;
+  }
+  trackByFn(index: number, item: any) {
+    return item.id; // Usa una propiedad única del usuario si es posible
+  }
+  selectedUser: any;
+
+  selectRow(user: any) {
+    this.selectedUser = user;
+  }
 }
