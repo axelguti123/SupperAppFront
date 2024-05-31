8f088333a7160905e7dfe0c212a40b12d684b148
pc <75484126@continental.edu.pe>
2024-05-31 08:10:36 -0500

    Creando partida

--- Back-End/SuperApp/SupperApp.Models/obj/Debug/net8.0/SupperApp.Models.AssemblyInfo.cs
+++ Back-End/SuperApp/SupperApp.Models/obj/Debug/net8.0/SupperApp.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupperApp.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2878e43bf23cd540cdf032fd61b91c673e21a886")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e2be126a93e6ca41b87c36ebc53dca3459cf05")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupperApp.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupperApp.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
