f2388cf2ef5c83585a3b1851fdfa5a49f7456295
Axel <axelguty@hotmail.com>
2024-06-03 17:19:25 -0500

    Seguimos arreglando interfaz de partida

--- Back-End/SuperApp/SuperApp.AccesoDatos/obj/Debug/net8.0/SuperApp.AccesoDatos.AssemblyInfo.cs
+++ Back-End/SuperApp/SuperApp.AccesoDatos/obj/Debug/net8.0/SuperApp.AccesoDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperApp.AccesoDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe98b4ae07cf7b9c1f9006c82de19da1ef8316d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648b451376cbe9c6e148435176f15cfd56337857")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperApp.AccesoDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperApp.AccesoDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
